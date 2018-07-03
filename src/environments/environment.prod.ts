b449f7a69bd96ab9b3648e1d89155a0be100a345
Alfonso Médicis <alfmedis@gmail.com>
2018-07-03 18:01:46 -0500

    Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyC5J-Czl-_HPpj9QI7RrKqcNhiUKwqPsGs",
+    authDomain: "loginapp-d232c.firebaseapp.com",
+    databaseURL: "https://loginapp-d232c.firebaseio.com",
+    projectId: "loginapp-d232c",
+    storageBucket: "loginapp-d232c.appspot.com",
+    messagingSenderId: "453771792427"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyC5J-Czl-_HPpj9QI7RrKqcNhiUKwqPsGs",
+    authDomain: "loginapp-d232c.firebaseapp.com",
+    databaseURL: "https://loginapp-d232c.firebaseio.com",
+    projectId: "loginapp-d232c",
+    storageBucket: "loginapp-d232c.appspot.com",
+    messagingSenderId: "453771792427"
+  }
 };
 
 /*
